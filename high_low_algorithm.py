e890b679a2f8042fc3aaea6ef05d9f186c0a660e
Bronson <bronson.hill@yahoo.com.au>
2022-12-20 11:26:05 +1100

    new file:   .DS_Store 	new file:   Price_data/.DS_Store 	new file:   Price_data/EURUSD=X.csv/.DS_Store 	new file:   Price_data/EURUSD=X.csv/15m.csv 	new file:   Price_data/EURUSD=X.csv/1d.csv 	new file:   Price_data/EURUSD=X.csv/1h.csv 	new file:   Price_data/EURUSD=X.csv/1m.csv 	new file:   Price_data/EURUSD=X.csv/1mo.csv 	new file:   Price_data/EURUSD=X.csv/1wk.csv 	new file:   Price_data/EURUSD=X.csv/2m.csv 	new file:   Price_data/EURUSD=X.csv/30m.csv 	new file:   Price_data/EURUSD=X.csv/5m.csv 	new file:   candlestick chart.py 	new file:   high_low_algorithm.py 	new file:   notes.txt 	new file:   price_data.csv 	new file:   yahoo_finance.py

---
+++ high_low_algorithm.py
@@ -0,0 +1,59 @@
+import csv
+import pandas as pd
+
+
+def import_data(file_name, ma=0):
+    df = pd.read_csv(file_name)
+    df.drop(df.columns[[5, 6, 7, 8]], axis=1, inplace=True)
+
+    ma_data = [0] * ma
+    average_price = []
+    trailing_prices = [0] * ma
+
+    rows = df.itertuples()
+    i = 0
+    for row in rows:
+        sum_price = float(row[2]) + float(row[3]) + float(row[4]) + float(row[5])
+        average_price.append(sum_price/4)
+        
+        if i >= ma:
+            average = sum(trailing_prices)/ma
+            ma_data.append(float(average))
+
+        i += 1
+        trailing_prices.insert(0, average_price[-1])
+        trailing_prices.pop()
+
+    df.drop('Open', axis=1)
+    df.drop('Close', axis=1)
+
+    df[f'{ma}MA'] = ma_data
+
+    return df
+
+
+print(import_data('Price data/CL=F_15m.csv', 4))
+
+
+def detect_trend(file_name, ma=4):
+    '''from ohlc data, returns a list of booleans: True when uptrending
+    and False when downtrending'''
+    
+    trend = []
+    df = import_data(file_name, ma)
+    
+    rows = df.itertuples()
+    prev_row = next(rows)
+    i = 0
+    while i < len(df):
+        row = next(rows)
+        
+        if prev_row > row:
+            trend.append(False)
+        else:
+            trend.append(True)
+        
+        prev_row = row
+
+    
+    return df
---
+++ yahoo_finance.py
@@ -0,0 +1,103 @@
+import datetime
+import yfinance
+import pandas as pd
+import os
+
+HISTORY_LIMIT = {'1m': 30, '2m': 60, '5m': 60, '15m': 60, '30m': 60, 
+'1h': 730, '1d': 730, '1wk': 10000, '1mo': 300}
+
+REQUEST_LIMIT = {'1m': 7, '2m': 60, '5m': 60, '15m': 60, '30m': 60, 
+'1h': 730, '1d': 730, '1wk': 10000, '1mo': 10000}
+
+
+def fetch_ohlc_data(ticker, interval, start, end):
+    '''fetches ohlc data for ticker for interval and start and end time'''
+
+    ticker = yfinance.Ticker(ticker)
+    df = ticker.history(start=start, end=end, interval=interval)
+
+    return df
+
+
+def file_exists(dir):
+    '''tests whether price data file already exists and returns last 
+    price date if so, otherwise returns false'''
+    
+    if os.path.exists(dir):
+        file = pd.read_csv(dir)
+
+        for elem in ['1d', '1wk', '1mo']:
+            if elem in dir:
+                format_str = "%Y-%m-%d"
+                break
+            else:
+                format_str = "%Y-%m-%d %H:%M:%S"
+
+        recent_date = datetime.datetime.strptime(
+            file.iloc[1:2,0:1].values[0][0],format_str) \
+            + datetime.timedelta(hours=16, minutes=1)
+
+        return recent_date
+    
+    else:
+        return False
+
+
+
+def compile_data(ticker, interval):
+    '''adds requested data to price data folder as csv'''
+    
+    
+    if not os.path.exists(f'Price_data/{ticker}.csv'):
+        os.makedirs(f'Price_data/{ticker}')
+
+    #dir = f'/Users/home/Library/Mobile Documents/com~apple~CloudDocs/Estate/Trading/Price_data/EURUSD=X.csv/{interval}.csv'
+    dir = f'Price_data/{ticker}.csv/{interval}.csv'
+
+    # sets start date of request according to whether file exists or not
+    exist_bool = file_exists(dir)
+    if exist_bool:
+        start = exist_bool
+    
+    # sets start date to oldest date allowed by yfinance
+    else:
+        try:
+            start = datetime.datetime.now() - datetime.timedelta(
+            days=HISTORY_LIMIT[interval])
+
+        except:
+            print(f'Try interval: {[interval for interval in HISTORY_LIMIT.keys()]}')
+            return
+    
+    df = pd.DataFrame()
+    
+    # makes request for data and appends to pandas dataframe
+    end = start
+    while end < datetime.datetime.today():
+        end = start + datetime.timedelta(days=REQUEST_LIMIT[interval])
+        if not df.empty:
+            df = pd.concat([df, fetch_ohlc_data(ticker, interval, start, end)])
+        else:
+            df = pd.DataFrame(fetch_ohlc_data(ticker, interval, start, end))
+        start = end
+    
+    # either appends dataframe to existing csv or creates new one
+    if exist_bool:
+        df.to_csv(dir, mode='a', index=True, header=False)
+    else:
+        df.to_csv(dir)
+    
+    return
+
+
+def update_ticker_data(ticker):
+    '''compiles data for all intervals for certain pairs'''
+    for interval in HISTORY_LIMIT.keys():
+        print(f'Compiling {interval} data for {ticker}.')
+        compile_data(ticker, interval)
+    print('Complete')
+
+    return
+
+
+update_ticker_data('EURUSD=X')
