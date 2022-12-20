34065f2c430f4941889f3993878e2598c0955812
Bronson <bronson.hill@yahoo.com.au>
2022-12-20 15:00:26 +1100

    modified:   high_low_algorithm.py 	deleted:    notes.txt

--- high_low_algorithm.py
+++ high_low_algorithm.py
@@ -2,48 +2,43 @@ import csv
 import pandas as pd
 
 
-def import_data(file_name, ma=0):
-    '''collects specified price data from database and adds the '''
+def import_data(file_name, ma=4):
+    '''collects specified price data from database and adds a column
+    with sma'''
     
     df = pd.read_csv(file_name)
     df.drop(df.columns[[5, 6, 7, 8]], axis=1, inplace=True)
 
-    ma_data = [0] * ma
-    average_price = []
-    trailing_prices = [0] * ma
-
-    rows = df.itertuples()
-    i = 0
-    for row in rows:
-        sum_price = float(row[2]) + float(row[3]) + float(row[4]) + float(row[5])
-        average_price.append(sum_price/4)
-        
-        if i >= ma:
-            average = sum(trailing_prices)/ma
-            ma_data.append(float(average))
-
-        i += 1
-        trailing_prices.insert(0, average_price[-1])
-        trailing_prices.pop()
-
     df.drop('Open', axis=1)
     df.drop('Close', axis=1)
-
-    df[f'{ma}MA'] = ma_data
 
     return df
 
 
-def add_sma(price_series, length):
-    '''from price series data adds '''
-    return price_series.rolling(length).mean()
+def averaged_price(df):
+    '''mutates df to have column of averaged open, high, low and close'''
+    average_price = []
+
+    rows = df.itertuples()
+    for row in rows:
+        sum_price = float(row[2]) + float(row[3]) + float(row[4]) + float(row[5])
+        average_price.append(sum_price/4)
+    
+    df['Price'] = average_price
+    return df
 
 
-print(add_sma(import_data('Price_data/EURUSD=X/15m.csv', 4).loc[:,'Close'], 5))
+def add_sma(df, length):
+    '''from price df adds a column of SMA with specified length'''
+    df[f'{length}SMA'] = df['Price'].rolling(length).mean()
+    return df
+
+
+# print(add_sma(import_data('Price_data/EURUSD=X/15m.csv', 4).loc[:,'Close'], 5))
 
 # print(import_data('Price_data/EURUSD=X/15m.csv', 4))
 
-
+print(add_sma(averaged_price(import_data('Price_data/EURUSD=X/15m.csv')), 4))
 
 
 
